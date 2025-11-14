600928cf8d3ca855302d165f3b263151ec385b14
TiagoP460 <tiagopi460@gmail.com>
2025-11-14 14:18:05 -0300

    o

--- jogo-aula-IFRJ-main/jogo-aula-IFRJ-main/Assets/Scripts/DeathZoneController.cs
+++ jogo-aula-IFRJ-main/jogo-aula-IFRJ-main/Assets/Scripts/DeathZoneController.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathZoneController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            ReloadCurrentScene();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ReloadCurrentScene()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
